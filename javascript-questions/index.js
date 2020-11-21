9814c000b11d248cdecc0e2df2ebb72cb71e0711
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-11-21 10:20:49 +0530

    Completion of the JavaScript Assignment

--- javascript-questions/index.js
+++ javascript-questions/index.js
@@ -1,4 +1,9 @@
-const superHeroes = require('./super-heroes.json');
+const superHeroes = require("./super-heroes.json");
+
+const PUBLISHERS = {
+  DC_Comics: "DC Comics",
+  Marvel_Comics: "Marvel Comics",
+};
 
 /**
  * Will find all of the marvel characters in the data and return them as an array
@@ -10,8 +15,10 @@ const superHeroes = require('./super-heroes.json');
  * ]
  */
 const getDCHeroes = (heroes) => {
-  return heroes;
-}
+  return heroes
+    .filter((hero) => hero.publisher === PUBLISHERS.DC_Comics)
+    .map((dcHero) => ({ ...dcHero, publisher: "DC" }));
+};
 
 /**
  * Converts the character property into an array
@@ -27,8 +34,13 @@ const getDCHeroes = (heroes) => {
  * ]
  */
 const convertCharactersToArray = (heroes) => {
-  return heroes;
-}
+  return heroes.map((hero) => {
+    return {
+      ...hero,
+      characters: hero.characters.split(","),
+    };
+  });
+};
 
 /**
  * Coverts super hero data so it is grouped the publisher property
@@ -44,8 +56,11 @@ const convertCharactersToArray = (heroes) => {
  * }
  */
 const groupByPublisher = (heroes) => {
-  return heroes;
-}
+  return heroes.reduce((result, hero) => {
+    result[hero.publisher] = [...(result[hero.publisher] || []), { ...hero }];
+    return result;
+  }, {});
+};
 
 /**
  * Returns and array of DC Comic heroes that have more than 1 character
@@ -61,12 +76,21 @@ const groupByPublisher = (heroes) => {
  * ]
  */
 const getDCHeroesWithMoreThanOneCharacter = (heroes) => {
-  return heroes;
-}
+  return heroes.filter(hero=>hero.publisher===PUBLISHERS.DC_Comics && hero.characters.split(",").length>1 ).map(dcHero=>({
+    ...dcHero,
+    characters:dcHero.characters.split(",")
+  }))
+};
 
 module.exports = {
   convertCharactersToArray,
   getDCHeroes,
   getDCHeroesWithMoreThanOneCharacter,
   groupByPublisher,
-}
+};
+
+/* Logging the results for verification */
+// console.log(getDCHeroes(superHeroes));
+// console.log(convertCharactersToArray(superHeroes))
+// console.log(groupByPublisher(superHeroes))
+// console.log(getDCHeroesWithMoreThanOneCharacter(superHeroes))
